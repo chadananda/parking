698c7b26b4c043a1c1bd056eeec96f622c824c9e
Chad <chadananda@gmail.com>
2015-04-11 11:03:39 -0700

    logging

--- update-parking.php
+++ update-parking.php
@@ -1,5 +1,11 @@
 <?php
 
+ini_set("log_errors", 1);
+ini_set("error_log", "php-error.log");
+
+
+error_log( "Update started!" );
+
 // Use in the "Post-Receive URLs" section of your GitHub repo.
 
 if ( $_POST['payload'] ) {
