30e3ec350bfa1d3b63cb43d76260550491666b6b
Chad <chadananda@gmail.com>
2015-04-11 11:08:57 -0700

    update parker

--- update-parking.php
+++ update-parking.php
@@ -1,10 +1,10 @@
 <?php
 
 ini_set("log_errors", 1);
-ini_set("error_log", "php-error.log");
+//ini_set("error_log", "php-error.log");
 
 
-error_log( "Update started!" );
+echo ( "Update started!" );
 
 // Use in the "Post-Receive URLs" section of your GitHub repo.
 
