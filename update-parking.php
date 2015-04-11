0d393b6fbf5b62adba44b0c1e239b70afb575f54
Chad <chadananda@gmail.com>
2015-04-11 11:21:47 -0700

    update parker

--- update-parking.php
+++ update-parking.php
@@ -7,7 +7,7 @@ ini_set("log_errors", 1);
 echo ( "Update started!" );
 
 // Use in the "Post-Receive URLs" section of your GitHub repo.
-if ( $_REQUEST['payload'] ) {
+//if ( $_REQUEST['payload'] ) {
   chdir($_SERVER['PHP_SELF']);
   exec('git reset --hard HEAD && git pull');
-} else echo (print_r($_REQUEST, true));
+//} else echo (print_r($_REQUEST, true));
