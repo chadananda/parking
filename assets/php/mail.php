dca1e6077a537dda4a947eddd263dfb9b1fafa0c
Chad <chadananda@gmail.com>
2015-04-16 07:36:12 -0700

    modified email sender script

--- assets/php/mail.php
+++ assets/php/mail.php
@@ -1,13 +1,13 @@
 <?php
 
-ini_set('display_errors',1);
-ini_set('display_startup_errors',1);
-error_reporting(-1);
+//ini_set('display_errors',1);
+//ini_set('display_startup_errors',1);
+//error_reporting(-1);
 
 /* =====================================================
  * change $email_to and $email_form
  * ===================================================== */
-$email_to = "root"; // the email address to which the form sends submissions
+$email_to = "chadananda@gmail.com"; // the email address to which the form sends submissions
 $email_from = "parking@aahh.com"; // the email address used as "From" when submissions are sent to the $email_to above (important that it has the same domain as the domain of your site - unless you have configured your server's mail settings)
 $email_subject = "Parking Inquiry Contact Form submitted";
 
