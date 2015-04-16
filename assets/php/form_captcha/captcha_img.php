4ef0bff873de3d187200239870686d1282f51648
Chad <chadananda@gmail.com>
2015-04-16 08:09:50 -0700

    simplified captcha code

--- assets/php/form_captcha/captcha_img.php
+++ assets/php/form_captcha/captcha_img.php
@@ -7,7 +7,7 @@
 
 session_start(); // Staring Session
 $captchanumber = '12356788889999990'; // Initializing PHP variable with string
-$captchanumber = substr(str_shuffle($captchanumber), 0, 4) . '99'; // Getting first 6 word after shuffle.
+$captchanumber = substr(str_shuffle($captchanumber), 0, 4) . '9'; // Getting first 6 word after shuffle.
 $_SESSION["captcha_code"] = $captchanumber; // Initializing session variable with above generated sub-string
 
 
